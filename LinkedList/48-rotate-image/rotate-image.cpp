64197b34eb8e5d2af88b9594fefbaf6dc1837a30
Bobble Code <woeyd261@gmail.com>
2026-05-05 01:36:40 +0800

    Time: 0 ms (100.00%) | Memory: 10.1 MB (92.70%) - LeetSync

---
+++ LinkedList/48-rotate-image/rotate-image.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    static inline void SWAP(int& x, int& y){
+        int t = x;
+        x = y;
+        y = t;
+    }
+
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+
+        for (int i = 0; i < n; i++){
+            for (int j = i; j < n; j++){
+                SWAP(matrix[i][j], matrix[j][i]);
+            }
+        }
+
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n/2; j++){
+                swap(matrix[i][j], matrix[i][n-j-1]);
+            }
+        }
+    }
+};
