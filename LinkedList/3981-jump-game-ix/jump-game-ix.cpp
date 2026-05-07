a75175ae86d95ed5698e471173dad074a5115238
Bobble Code <woeyd261@gmail.com>
2026-05-07 23:24:40 +0800

    Time: 2 ms (96.81%) | Memory: 225.6 MB (53.19%) - LeetSync

---
+++ LinkedList/3981-jump-game-ix/jump-game-ix.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> maxValue(vector<int>& nums) {
+        int m = nums.size();
+        vector<int> ans(m, 0);
+        vector<int> premax(m, -INT_MAX);
+        vector<int> surmin(m, INT_MAX);
+
+        premax[0] = nums[0];
+        for (int i = 1; i < m; i++){
+            premax[i] = max(nums[i], premax[i-1]);
+        }
+
+        surmin[m-1] = nums[m-1];
+        for (int i = m-2; i >= 0; i--){
+            surmin[i] = min(nums[i], surmin[i+1]);
+        }
+
+
+        ans[m-1] = premax[m-1];
+        for (int i = m-2; i >= 0; i--){
+            if (premax[i] > surmin[i+1]){
+                ans[i] = ans[i+1];
+            }
+            else{
+                ans[i] = premax[i];
+            }
+        }
+
+        return ans;
+    }
+};
